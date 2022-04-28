ca0dbf0ef9daff68831641b2bba53c550c659136
Michael Chen <michael.chen.1120@gmail.com>
2022-04-28 16:13:29 -0400

    Add record visit

--- URLShortener/app/models/visit.rb
+++ URLShortener/app/models/visit.rb
@@ -9,4 +9,8 @@ class Visit < ApplicationRecord
         class_name: :ShortenedUrl,
         foreign_key: :shortened_url_id,
         primary_key: :id
+
+    def self.record_visit!(user, shortened_url)
+        Visit.create!(user_id: user.id, shortened_url_id: shortened_url.id)
+    end
 end
